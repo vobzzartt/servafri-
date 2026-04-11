4acb806eb57d35ceb56fbc3eaa00f2b4a94c9e66
Victor Bodude <victorbodude@gmail.com>
2026-04-11 14:07:22 +0100

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://usenubis.com';
+  const baseUrl = 'https://servafri.com';
   const currentDate = new Date().toISOString();
 
   return [
